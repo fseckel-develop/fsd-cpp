fd1feaffdb9110e450a6e06fbe15d9378acee8b1
Franz Seckel <fseckel.develop@gmail.com>
2025-11-04 00:34:09 +0100

    feat(core): add stack

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 // Created by Franz Seckel on 08.10.25.
 #include <iostream>
 #include <ostream>
-#include "src/Deque.h"
+#include "src/DoubleList.h"
+#include "src/Stack.h"
 
 int main() {
-    const Deque deque = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::cout << deque.size() << std::endl;
+    Stack<int, DoubleList<int>> stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    std::cout << stack.top() << std::endl;
+    stack.pop();
+    std::cout << stack.top() << std::endl;
+    stack.pop();
+    std::cout << stack.top() << std::endl;
 }
---
+++ src/Stack.h
@@ -0,0 +1,46 @@
+// Created by Franz Seckel on 03.11.25.
+#pragma once
+#include <concepts>
+#include "Vector.h"
+
+template <typename C, typename T>
+concept StackContainer = requires(C container, const C constContainer, const T& value) {
+    { container.pushBack(value) } -> std::same_as<void>;
+    { container.popBack() } -> std::same_as<void>;
+    { constContainer.back() } -> std::convertible_to<T>;
+    { constContainer.empty() } -> std::convertible_to<bool>;
+};
+
+template <typename T, StackContainer<T> C = Vector<T>>
+class Stack {
+public:
+    // Aliases:
+    using valueType = T;
+    using constReference = const valueType&;
+    using sizeType = size_t;
+    using containerType = C;
+
+    // Explicit Constructors:
+    Stack() = default;
+    explicit Stack(const containerType& container) : container_(container) {}
+    explicit Stack(containerType&& container) noexcept(std::is_nothrow_move_constructible_v<containerType>) :
+        container_(std::move(container)) {
+    }
+
+    // Capacity:
+    sizeType size() const { return container_.size(); }
+    bool empty() const { return container_.isEmpty(); }
+
+    // Element Access:
+    constReference top() const { return container_.back(); }
+
+    // Modifiers:
+    void push(const valueType& value) { container_.pushBack(value); }
+    void pop() { container_.popBack(); }
+    void swap(Stack& other) noexcept(std::is_nothrow_swappable_v<containerType>) {
+        std::swap(container_, other.container_);
+    }
+
+private:
+    containerType container_;
+};
