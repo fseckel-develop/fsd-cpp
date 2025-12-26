b76dab81856ab445255bc5699510e2f1dfcabeb4
Franz Seckel <fseckel.develop@gmail.com>
2025-12-26 17:41:37 +0100

    feat(math): add vector

---
+++ src/algebra/vector/vector_fwd.hpp
@@ -0,0 +1,11 @@
+// Created by Franz Seckel on 19.12.2025.
+#pragma once
+#include <cstddef>
+
+namespace fs::math::algebra
+{
+    /// Forward-declaration for the vector type
+    template<typename valueType, std::size_t N>
+    struct Vector;
+
+} // namespace fs::math::algebra
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+// Created by Franz Seckel on 17.12.2025.
+#include <iostream>
+#include "algebra/vector.hpp"
+
+using namespace fs::math::algebra;
+
+int main() {
+    constexpr Vector3 translation = {1.0f, 2.0f, 3.0f};
+    constexpr Vector3 rotationAxis = {0.0f, 1.0f, 0.0f};
+    constexpr Vector3 scaleFactors = {3.0f, 4.0f, 0.5f};
+    std::cout << "Translation:   " << translation << std::endl;
+    std::cout << "Rotation axis: " << rotationAxis << std::endl;
+    std::cout << "Scale along X: " << scaleFactors.x() << std::endl;
+    std::cout << "Scale along Y: " << scaleFactors.y() << std::endl;
+    std::cout << "Scale along Z: " << scaleFactors.z() << std::endl;
+}
