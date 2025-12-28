12b1cf0d9656e8def9edc7e9cd313e3a619fafac
Franz Seckel <fseckel.develop@gmail.com>
2025-12-28 01:11:38 +0100

    feat(math): add linear transform

---
+++ src/algebra/linear_transform.hpp
@@ -0,0 +1,4 @@
+// Created by Franz Seckel on 27.12.2025.
+#pragma once
+
+
---
+++ src/algebra/linear_transform/linear_transform_aliases.hpp
@@ -0,0 +1,15 @@
+// Created by Franz Seckel on 27.12.2025.
+#pragma once
+#include "linear_transform.hpp"
+
+namespace fs::math::algebra
+{
+    using LinearTransform2 = LinearTransform<float,2>;
+    using LinearTransform3 = LinearTransform<float,3>;
+    using LinearTransform4 = LinearTransform<float,4>;
+
+    using LinearTransform2d = LinearTransform<double,2>;
+    using LinearTransform3d = LinearTransform<double,3>;
+    using LinearTransform4d = LinearTransform<double,4>;
+
+} // namespace fs::math::algebra
